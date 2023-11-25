43a4a96d90e3fc9e4017b8fc6b22011430d58ef0
saiarun081 <105878355+saiarun081@users.noreply.github.com>
2023-11-25 12:28:45 -0600

    Create Matrix column sum.java

---
+++ Matrix column sum.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[][] = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        Solution sol = new Solution();
+        sol.matrixSum(a);
+    }
+    
+    public void matrixSum(int arr[][]) {
+        int n = arr.length;
+        int m = arr[0].length;
+
+        // Calculate and print column-wise sum
+        for (int j = 0; j < m; j++) {
+            int rowSum = 0;
+            for (int i = 0; i < n; i++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println(rowSum);
+        }
+    }
+}
