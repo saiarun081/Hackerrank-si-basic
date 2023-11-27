be3cf815e5b556f93b000cc0ffc58d330f4831a6
saiarun081 <105878355+saiarun081@users.noreply.github.com>
2023-11-27 12:43:31 -0600

    Create Sum of two matrices.java

---
+++ Sum of two matrices.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[][] = new int[n][m];
+        int b[][] = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j]+b[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
